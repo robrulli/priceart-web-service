6328052ca875d40bd4d7ace64e565a03e5970ebd
Nicola La Rocca <nicola.larocca@xantrix.it>
2018-10-08 12:08:01 +0200

    altri

--- src/test/java/com/xantrix/webapp/UnitTest/RepositoryTest/PrezziRepositoryTest.java
+++ src/test/java/com/xantrix/webapp/UnitTest/RepositoryTest/PrezziRepositoryTest.java
@@ -13,9 +13,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
+@TestPropertySource(locations="classpath:application-list1.properties")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class)
 @SpringBootTest
