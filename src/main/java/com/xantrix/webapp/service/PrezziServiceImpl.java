4482cb4403bd217c82b19f598eda9e85dbec87e7
Nicola La Rocca <nicola@kalsyfer.com>
2018-10-11 11:04:02 +0200

    Aggiunti Swagger e Caching

--- src/main/java/com/xantrix/webapp/service/PrezziServiceImpl.java
+++ src/main/java/com/xantrix/webapp/service/PrezziServiceImpl.java
@@ -3,6 +3,9 @@ package com.xantrix.webapp.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import com.xantrix.webapp.entities.DettListini;
@@ -16,18 +19,23 @@ public class PrezziServiceImpl implements PrezziService
 	PrezziRepository prezziRepository;
 
 	@Override
+	@Cacheable(value = "chacheprezzo",key = "#CodArt",sync = true)
 	public DettListini SelPrezzo(String CodArt, String Listino)
 	{
 		return prezziRepository.findByCodArtAndIdList(CodArt, Listino);
 	}
 
 	@Override
+	@Caching(evict = { 
+		@CacheEvict(cacheNames="chacheprezzo",key = "#dettListini.codArt")})
 	public void InsPrezzo(DettListini dettListini) 
 	{
 		prezziRepository.save(dettListini);
 	}
 
 	@Override
+	@Caching(evict = { 
+		@CacheEvict(cacheNames="chacheprezzo",key = "#CodArt")})
 	public void DelPrezzo(String CodArt, String IdList) 
 	{
 		prezziRepository.DelRowDettList(CodArt, IdList);
